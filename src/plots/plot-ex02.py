26e5e8e83b6539ad1f7dda003802ae325cb30bbc
Sanchi Vaishnavi <sanchi.vaishnavi@stud.uni-heidelberg.de>
2024-06-19 14:54:55 +0200

    Change logic in plotter
    * Solution to Generalized* is compared with the very accurate Arpack

--- src/plots/plot-ex02.py
+++ src/plots/plot-ex02.py
@@ -6,22 +6,45 @@ plt.rcParams["figure.figsize"] = (10, 10)
 plt.rcParams['figure.constrained_layout.use'] = True
 
 # Export data
-def plot_frame(ax, status, nev, submethod):
+def plot_frame(ax, status, nev, submethod, matrix):
     match status:
-        case 'tol':
-            filename = ""
-            for x in map(str, np.arange(1,6)):
-                filename = 'load'+x
-                filename = np.loadtxt('../measurements/N_40k_m_32_maxiter_4k_shift_1e-3_tol_2e-'+x+'_overlap_3_method_std_submethod_'+submethod, delimiter=' ',comments='#')
-                ax.semilogy(np.arange(nev),filename[:,5],label='2e-'+x,marker='')
+        case 'tol-std':
+            for x in map(str, np.arange(1,5)):
+                for y in map(str, np.arange(1,5)):
+                    filename = '../measurements/N_40k_m_32_maxiter_4k_shift_1e-3_tol_'+y+'e-'+x+'_overlap_3_method_std_submethod_'+submethod+'_'+matrix
+                    dataset = np.loadtxt(filename, delimiter=' ',comments='#')
+                    itr = find_iterations(filename)
+                    ax.semilogy(np.arange(nev),dataset[:,-1],label=y+'e-'+x+', itr: '+itr,marker='')
 
             # Create legend
-            ax.set_title('method: '+submethod)
-            ax.set_aspect('equal','box')
+            ax.set_title('problem: ' +matrix +', method: '+submethod)
+            ax.set_aspect('auto','box')
+            labelLines(ax.get_lines(),align=False,zorder=6.5)
+
+        case 'tol-gen':
+            for x in map(str, np.arange(1,5)):
+                for y in map(str, np.arange(1,5)):
+                    filename = '../measurements/N_40k_m_32_maxiter_4k_shift_1e-3_tol_'+y+'e-'+x+'_overlap_3_method_gen_submethod_'+submethod+'_'+matrix
+                    dataset = np.loadtxt(filename, delimiter=' ',comments='#')
+                    itr = find_iterations(filename)
+                    ax.semilogy(np.arange(nev),dataset[:,-2],label=y+'e-'+x+', itr: '+itr,marker='')
+
+            # Create legend
+            ax.set_title('problem: ' +matrix +', method: '+submethod)
+            ax.set_aspect('auto','box')
             labelLines(ax.get_lines(),align=False,zorder=6.5)
 
         case _:
             return "Try again!"
+
+def find_iterations(filename):
+    with open(filename) as f:
+        datafile = f.readlines()
+    itr = 0
+    for line in datafile:
+        if "iterations=" in line:
+            itr = line.partition("iterations=")[2]
+    return itr
 
 def beautify(fig, title, xlabel, ylabel, location):
     fig.suptitle(title)
@@ -31,14 +54,27 @@ def beautify(fig, title, xlabel, ylabel, location):
 
 def main():
     nev = 32
+    matrix = "dir"
     fig, (ax1, ax2) = plt.subplots(2, 1, sharex=False)
-    plot_frame(ax1, 'tol', nev, 'ftw')
-    plot_frame(ax2, 'tol', nev, 'stw')
+    plot_frame(ax1, 'tol-std', nev, 'ftw', matrix)
+    plot_frame(ax2, 'tol-std', nev, 'stw', matrix)
 
     title ='Accuracy of computed eigenvalues given various error tolerances \n n:40000, m:32, shift: 1e-3, overlap: 3'
     xlabel='Eigenvalues ranked smallest to largest'
     ylabel='Error in eigenvalues'
-    location='img/tol.pdf'
+    location='img/tol-std-'+matrix+'.pdf'
+    beautify(fig,title,xlabel,ylabel,location)
+
+    nev = 32
+    matrix = "neu"
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=False)
+    plot_frame(ax1, 'tol-gen', nev, 'ftw', matrix)
+    plot_frame(ax2, 'tol-gen', nev, 'stw', matrix)
+
+    title ='Accuracy of computed eigenvalues given various error tolerances \n n:40000, m:32, shift: 1e-3, regularization: 0, overlap: 3'
+    xlabel='Eigenvalues ranked smallest to largest'
+    ylabel='Error in eigenvalues'
+    location='img/tol-gen-'+matrix+'.pdf'
     beautify(fig,title,xlabel,ylabel,location)
 
 if __name__ == "__main__":
--- src/runscript.sh
+++ src/runscript.sh
@@ -2,6 +2,7 @@
 
 method="$1"
 submethod="$2"
+matrix="$3"
 SRCPATH=/Users/vsanc/Documents/1UHD/7-wise-22-23/TRYIT/DUNE/dune-eigensolver/src
 BINPATH=/Users/vsanc/Documents/1UHD/7-wise-22-23/TRYIT/DUNE/release-build/dune-eigensolver/src
 FILE=$SRCPATH/dune-eigensolver.ini
@@ -23,10 +24,12 @@ fi
 
 make dune-eigensolver
 
-TOL="1 2 3 4 5"
+TOL="1 2 3 4"
 for val in $TOL; do
-  sed -i "" "s/^tol.*/tol = 2e-$val/" $FILE
-  echo "2e-$val $1 $2"
-  ./dune-eigensolver &> $SRCPATH/measurements/N_40k_m_32_maxiter_4k_shift_1e-3_tol_2e-"$val"_overlap_3_method_"$1"_submethod_"$2"
+  for x in $TOL; do
+    sed -i "" "s/^tol.*/tol = "$x"e-"$val"/" $FILE
+    echo ""$x"e-"$val" $1 $2 $3"
+    ./dune-eigensolver &> $SRCPATH/measurements/N_40k_m_32_maxiter_4k_shift_1e-3_tol_"$x"e-"$val"_overlap_3_method_"$1"_submethod_"$2"_"$3"
+  done
 done
 cd -
