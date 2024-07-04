e9f8735c7917224d9cd49d9c33ef35c604c6d2ff
Sanchi Vaishnavi <sanchi.vaishnavi@stud.uni-heidelberg.de>
2024-07-04 17:12:03 +0200

    WIP: Add total runtime info in legends

--- src/plots/plot-ex02.py
+++ src/plots/plot-ex02.py
@@ -9,40 +9,53 @@ plt.rcParams['figure.constrained_layout.use'] = True
 def plot_frame(ax, status, nev, submethod, matrix):
     match status:
         case 'tol-std':
-            for y in map(str, np.arange(2,10,2)):
-                filename = '../measurements/N_40k_m_32_maxiter_4k_shift_1e-3_tol_'+y+'e-2_overlap_3_method_std_submethod_'+submethod+'_'+matrix
-                dataset = np.loadtxt(filename, delimiter=' ',comments='#')
-                itr = find_iterations(filename)
-                ax.semilogy(np.arange(nev),dataset[:,-1],label=y+'e-2, itr: '+itr,marker='')
+            for x in map(str, np.arange(1,3)):
+                for y in map(str, np.arange(2,10,2)):
+                    filename = '../measurements/N_40k_m_32_maxiter_4k_shift_1e-3_tol_'+y+'e-'+x+'_overlap_3_method_std_submethod_'+submethod+'_'+matrix
+                    dataset = np.loadtxt(filename, delimiter=' ',comments='#')
+                    itr = find_iterations(filename)
+                    time = find_time(filename)
+                    custom_label=y+'e-'+x+', itr: '+itr+'t: '+time
+                    ax.semilogy(np.arange(nev),dataset[:,-1],label=custom_label.partition("t: ")[0], marker='')
             for x in map(str, np.arange(1,5)):
                 for y in map(str, np.arange(1,2)):
                     filename = '../measurements/N_40k_m_32_maxiter_4k_shift_1e-3_tol_'+y+'e-'+x+'_overlap_3_method_std_submethod_'+submethod+'_'+matrix
                     dataset = np.loadtxt(filename, delimiter=' ',comments='#')
                     itr = find_iterations(filename)
-                    ax.semilogy(np.arange(nev),dataset[:,-1],label=y+'e-'+x+', itr: '+itr,marker='')
+                    time = find_time(filename)
+                    custom_label=y+'e-'+x+', itr: '+itr+'t: '+time
+                    ax.semilogy(np.arange(nev),dataset[:,-1],label=custom_label.partition("t: ")[0], marker='')
 
             # Create legend
             ax.set_title('problem: ' +matrix +', method: '+submethod)
             ax.set_aspect('auto','box')
-            labelLines(ax.get_lines(),align=False,zorder=6.5)
+            labelLines(ax.get_lines(),align=False,zorder=2)
+            #ax.legend(labels=[custom_label.partition("t: ")[2]])
+            #ax.legend()
 
         case 'tol-gen':
-            for y in map(str, np.arange(2,10,2)):
-                filename = '../measurements/N_40k_m_32_maxiter_4k_shift_1e-3_tol_'+y+'e-2_overlap_3_method_gen_submethod_'+submethod+'_'+matrix
-                dataset = np.loadtxt(filename, delimiter=' ',comments='#')
-                itr = find_iterations(filename)
-                ax.semilogy(np.arange(nev),dataset[:,-2],label=y+'e-2, itr: '+itr,marker='')
+            for x in map(str, np.arange(2,3)):
+                for y in map(str, np.arange(2,10,2)):
+                    filename = '../measurements/N_40k_m_32_maxiter_4k_shift_1e-3_tol_'+y+'e-'+x+'_overlap_3_method_gen_submethod_'+submethod+'_'+matrix
+                    dataset = np.loadtxt(filename, delimiter=' ',comments='#')
+                    itr = find_iterations(filename)
+                    time = find_time(filename)
+                    custom_label = y+'e-'+x+', itr: '+itr+'t: '+time
+                    ax.semilogy(np.arange(nev),dataset[:,-2],label=custom_label.partition("t: ")[0], marker='')
             for x in map(str, np.arange(1,5)):
                 for y in map(str, np.arange(1,2)):
                     filename = '../measurements/N_40k_m_32_maxiter_4k_shift_1e-3_tol_'+y+'e-'+x+'_overlap_3_method_gen_submethod_'+submethod+'_'+matrix
                     dataset = np.loadtxt(filename, delimiter=' ',comments='#')
                     itr = find_iterations(filename)
-                    ax.semilogy(np.arange(nev),dataset[:,-2],label=y+'e-'+x+', itr: '+itr,marker='')
+                    time = find_time(filename)
+                    custom_label = y+'e-'+x+', itr: '+itr+'t: '+time
+                    ax.semilogy(np.arange(nev),dataset[:,-2],label=custom_label.partition("t: ")[0], marker='')
 
             # Create legend
             ax.set_title('problem: ' +matrix +', method: '+submethod)
             ax.set_aspect('auto','box')
-            labelLines(ax.get_lines(),align=False,zorder=6.5)
+            labelLines(ax.get_lines(),align=False,zorder=2)
+            #ax.legend()
 
         case _:
             return "Try again!"
@@ -55,6 +68,16 @@ def find_iterations(filename):
         if "iterations=" in line:
             itr = line.partition("iterations=")[2]
     return itr
+
+def find_time(filename):
+    with open(filename) as f:
+        datafile = f.readlines()
+    time = 0
+    for line in datafile:
+        if "time_total" in line:
+            time = line.partition("time_total=")[2]
+            time = time.partition(" ")[0]
+    return time
 
 def beautify(fig, title, xlabel, ylabel, location):
     fig.suptitle(title)
