ed730fc7056892c2d735731a653bf490a924da64
Sanchi Vaishnavi <sanchi.vaishnavi@stud.uni-heidelberg.de>
2024-12-03 08:12:23 +0100

    Eigensolver in python

--- standard_stewart.py
+++ standard_stewart.py
@@ -2,10 +2,9 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from numpy import linalg as LA
-from scipy.sparse import diags, csr_matrix
-from scipy.sparse.linalg import spsolve, LaplacianNd
-from scipy.linalg import solve, lu, blas, orth, eigh
-import matplotlib.pyplot as plt
+from scipy.sparse import csr_matrix
+from scipy.sparse.linalg import LaplacianNd, eigsh
+from scipy.linalg import lu, blas, orth, eigh
 import math
 import sys
 
@@ -19,7 +18,7 @@ def get_laplacian_rhs(N, overlap, problem):
     nx = N
     ny = N
     n = nx*ny
-    dx = (x_max - x_min)/(n+1)                    
+    dx = (x_max - x_min)/(n+1)
     dy = (y_max - y_min)/(n+1)
     x = np.linspace(x_min+dx, x_max-dx, n)  # Grid points in x-direction
     y = np.linspace(y_min+dy, y_max-dy, n)  # Grid points in y-direction
@@ -41,7 +40,7 @@ def get_laplacian_rhs(N, overlap, problem):
     # Create the sparse matrix
     # A = diags([4*ones, -lower_diagonal, -upper_diagonal, -ones, -ones], offsets, shape=(n, n), dtype=float)
 
-    # TODO: Set up A correctly for dirichlet
+    # Set up A correctly for dirichlet
     # main_diag = np.ones(n)*4.0
     # side_diag = np.ones(n-1)
     # side_diag[np.arange(1,n)%nx==0] = 0
@@ -52,6 +51,7 @@ def get_laplacian_rhs(N, overlap, problem):
 
     A_helper = LaplacianNd(grid_shape=(nx,nx), boundary_conditions=problem,dtype=np.float64)
     A = np.dot(-1,A_helper.toarray())
+    # A = csr_matrix(A)
     # Construct the RHS
     # B = diags([ones,zeros,zeros,zeros,zeros], offsets, shape=(n, n), dtype=np.float64).toarray()
     if problem == 'dirichlet':
@@ -67,10 +67,10 @@ def get_laplacian_rhs(N, overlap, problem):
                 pu[k] = 0.0
             else:
                 pu[k] = 1.0
-        print(pu)
+        # print(pu)
         for itr, val in np.ndenumerate(B):
             B[itr] = val * pu[itr[0]] * pu[itr[1]]
-
+    # B = csr_matrix(B)
     #Solve the linear system using a sparse matrix solver
     # u = solve(Asdir, Bsdir, assume_a='pos').reshape(n, n)
     #Plot the solution
@@ -143,22 +143,32 @@ def orthonormalize(Q_rand,m):
     print("\nGram-Schmidt test for Q_rand: ", np.allclose(Q @ R, Q_rand))
     return Q
 
-def B_norm(B, x):
-    a = math.sqrt(np.dot(x, np.matmul(B,x)))
+def B_norm(M, x):
+    a = math.sqrt( x.T @ M @ x )
     return a
 
-def B_orthonormalize(B, Q, m):
-    #A = Q @NOTE creates a reference! not a by-value copy
-    A = Q.copy()
+def B_orthonormalize(B, A, m, verbose):
+    #Q = A @NOTE creates a reference! not a by-value copy
+    Q = np.zeros(A.shape, dtype=float)
     R = np.zeros((m,m),dtype=float)
-    for j in range(0, Q.shape[1]):
-        R[j,j] = B_norm(B,Q[:,j])
-        A[:,j] = Q[:,j]/R[j,j]
-        for k in range(j+1, Q.shape[1]):
-            R[j,k] = np.dot(A[:,j], np.matmul(B,Q[:,k]))
-            Q[:,k] = Q[:,k] - R[j,k]*A[:,j]
-    if (Q.shape[0] == Q.shape[1]):
-        print("\n||Q * x||_B = ||x||_B: ", B_norm(B,np.matmul(Q,np.ones(m))), B_norm(B,np.ones(m)))
+
+    for k in range(0, Q.shape[1]):
+        R[k,k] = B_norm(B, A[:,k])
+        Q[:,k] = A[:,k] / R[k,k]
+        for j in range(k+1, Q.shape[1]):
+            R[k,j] = Q[:,k].T @ B @ A[:,j]
+            A[:,j] = A[:,j] - Q[:,k] * R[k,j]
+
+    # for j in range(0, Q.shape[1]):
+    #     R[j,j] = B_norm(B, A[:,j])
+    #     Q[:,j] = A[:,j] / R[j,j]
+    #     for k in range(j+1, Q.shape[1]):
+    #         R[j,k] = Q[:,j].T @ A[:,k] # ADD THE APPROPRIATE SCALAR PRODUCT!
+    #         A[:,k] = A[:,k] - R[j,k] * Q[:,j]
+
+    # Test correctness of orthonormalization
+    if (Q.shape[0] == Q.shape[1] and verbose >=2):
+        print("\n||Q * x||_B = ||x||_B: ", B_norm(B, np.matmul(Q,np.ones(m))), B_norm(B, np.ones(m)))
         print("\nQ_T * B * Q = I", np.allclose(Q.T@(B@Q), np.eye(m)), '\n', Q.T@(B@Q),'\n')
         print("\nQ^-1 = Q_T * B_T: ", np.allclose(np.linalg.inv(Q), Q.T@B.T))
         for j in range(0,Q.shape[1]):
@@ -182,64 +192,100 @@ def stewart_orthogonalize_gen(A,B,m,shift,tol,maxiter,verbose):
     # print("A^-1 = R^-1 . Q^T ", np.allclose(np.linalg.inv(A), np.linalg.inv(A_R)@A_Q.T) )
     
     # setup randomized Q_rand
-    generator = np.random.Generator(np.random.MT19937(seed=400))
+    generator = np.random.Generator(np.random.MT19937(seed=800))
     Q_rand = generator.normal(0,1,size=(N**2,m))
 
-    # orthonormalize Q_rand
-    # Y = orthonormalize(Q_rand,m)
-    # print('\n',Y,'\n')
-
     # Orthonormalize Q with respect to the scalar product
-    Q = B_orthonormalize(B,Q_rand,m)
-    # print('\n',Q,'\n')
+    Q_orth = B_orthonormalize(B, Q_rand, m, verbose)
 
     # Loop:
-    #   1. X := A^-1 * X
-    #   2. Orthonormalize X
-    #   3. A_hat := X^T * A * X
-    #   3. B_hat := X^T * B * X
-    #   4. A_hat * S =: B * S * D
+    #   4. A_hat * S =: B_hat * S * D
     #       => A_hat = B_hat * S * D * S^-1, in most cases S will be invertible
     #   5. => D = (XS)^T * A * (XS) => X := XS
+    initial_partial_off = 0.0
     D = np.zeros(m)
     Q0 = np.zeros((N**2, m))
     Q1 = np.zeros((N**2, m))
     Q2 = np.zeros((N**2, m))
+    A_hat = np.zeros((m,m))
+    B_hat = np.zeros((m,m))
     i = 0
     while i <  maxiter:
-        # print("\nIteration: ", i,'\n')
-        # print(Q,'\n')
-        if np.iscomplexobj(Q):
+        if np.iscomplexobj(Q_orth):
             break
         else:
-            Q0 = np.matmul(B, Q)
-        Q1 = np.matmul(A_inv, Q0)
-        Q2 = B_orthonormalize(B,Q1,m)
-        A_hat = np.matmul(Q2.T, np.matmul(A, Q2))
-        B_hat = np.matmul(Q2.T, np.matmul(B, Q2))
-        D, S = eigh(A_hat,B_hat)
-        # Theor. eigenvalues are 1 +/- 1e-9
+            np.matmul(B, Q_orth, out=Q0)
+        np.matmul(A_inv, Q0, out=Q1)
+        Q2 = B_orthonormalize(B, Q1, m, verbose)
+
+        Q3 = np.zeros((N**2, m))
+        np.matmul(A, Q2, out=Q3)
+        np.matmul(Q2.T, Q3, out=A_hat)
+
+        Q4 = np.zeros((N**2, m))
+        np.matmul(B, Q2, out=Q4)
+        np.matmul(Q2.T, Q4, out=B_hat)
+
+        #Stewart Acceleration
+        np.linalg.cholesky(A_hat)
+        np.linalg.cholesky(B_hat)
+        D, S = eigh(A_hat, B_hat) # Theor. eigenvalues are 1 +/- 1e-9
         if verbose >= 2:
             print(S,'\n')
-        Q = np.matmul(Q2, S)
-        # print(Q,'\n')
+        np.matmul(Q2, S, out=Q_orth)
+
+        # Stopping criterion computation
+        partial_off = 0.0
+        partial_diag = 0.0
+        for j in range(0, math.ceil(m*0.75)):
+            for k in range(0,math.ceil(m*0.75)):
+                if j == k:
+                    partial_diag += A_hat[j,k] * A_hat[j,k]
+                elif j != k and i == 0:
+                    initial_partial_off += A_hat[j,k] * A_hat[j,k]
+                elif j != k:
+                    partial_off += A_hat[j,k] * A_hat[j,k]
+        if verbose >= 1:
+            print('iter: ',i,' norm_off: %.6e' % np.sqrt(partial_off),' norm_diag: %.6e' % np.sqrt(partial_diag),'initial_norm_off: %.6e' % np.sqrt(initial_partial_off))
+        # Stopping criterion condition
+        if i > 0 and np.sqrt(partial_off) < tol * np.sqrt(initial_partial_off):
+            break
         i+=1
+
     Eval = np.zeros((m),dtype=float)
     for i in range(m):
         Eval[i] = D[i] - shift
     Eveq = Q2.copy()
     
+    for i in range(0, m):
+        print("%.6e" % A_hat[i,i], ": ", "%.1e" % B_hat[i,i])
+
     return Eval, Eveq
 
+def arpack_backend(A, B, m, shift, tol, maxiter):
+    print('\n Arpack backend')
+    W, E = eigsh(A, m, B, sigma=shift, tol=tol, maxiter=maxiter)
+    # is_match = np.zeros((m),dtype=bool)
+    # for i in range(0,m):
+    #     # y = (W[i]) * np.matmul(B, E[:,i])
+    #     # z = np.matmul(A, E[:,i])
+    #     y = ((1/W[i]) * E[:,i])
+    #     z = np.matmul(np.linalg.inv(A), np.matmul(B, E[:,i])) 
+    #     is_match[i] = np.allclose(y,z)
+    # print(all(is_match))
+
+    return W, E
+
 def main():
-    N = 10
-    m = 4
+    N = 80 # 46
+    m = 10 # 10
     verbose = 1
     shift = 1e-3
     tol = 1e-2
     maxiter = 10
     overlap = 1
     np.set_printoptions(threshold=sys.maxsize,linewidth=2000,formatter={'float': lambda x: format(x, '6.6e')})
+
     #### Dirichlet ####
     # Asdir, Bsdir = get_laplacian_rhs(N, overlap, 'dirichlet')
     # if verbose >= 2: 
@@ -249,34 +295,38 @@ def main():
     # np.linalg.cholesky(Bsdir)
 
     # Eval_dir, Eveq_dir = stewart_orthogonalize_std(Asdir,m,shift,tol,maxiter,verbose)
-    # print('\n\n', np.sort(Eval_dir))
     # if verbose >= 2:
     #     print('\n\n', Eveq_dir)
-
-    # y = Eval_dir[-1] * Eveq_dir[:,-1]
-    # z = np.matmul(Asdir,Eveq_dir[:,-1])
-    # print(np.allclose(y,z))
-
     # print(np.allclose(Asdir @ Eveq_dir, Eveq_dir @ np.diag(Eval_dir)))
 
     #### Neumann ####
+    # Asneu = Asdir
+    # Bsneu = Bsdir
     Asneu, Bsneu = get_laplacian_rhs(N, overlap, 'neumann')
-    # print(np.linalg.cholesky(Asneu.toarray()))
-    # print(-Asneu.eigenvalues())
-    # https://stackoverflow.com/questions/5563743/check-for-positive-definiteness-or-positive-semidefiniteness
     if verbose >= 2:
         print (Asneu, '\n\n', Bsneu)
+    # np.linalg.cholesky(Asneu)
+    # np.linalg.cholesky(Bsneu)
     Eval_neu, Eveq_neu = stewart_orthogonalize_gen(Asneu, Bsneu, m, shift, tol, maxiter, verbose)
-    print('\n\n', Eval_neu)
     if verbose >= 2:
-        print('\n\n', Eveq_neu)
+        print(Eval_neu, '\n\n', Eveq_neu)
 
-    y = Eval_neu[1] * Eveq_neu[:,1]
-    z = np.matmul(Asneu,Eveq_neu[:,1])
-    
-    print(np.allclose(y,z))
-    # print(np.allclose(np.matmul(Asneu,Eveq_neu)- np.matmul(Eveq_neu, np.diag(Eval_neu)), np.zeros((N**2,m))))
-    Asneu = blas.daxpy(Bsneu, Asneu, a=shift)
-    print(np.allclose(Asneu @ Eveq_neu, Bsneu @ Eveq_neu @ (np.diag(Eval_neu)+shift)))
+    # Testing correctness
+    W, E = arpack_backend(Asneu, Bsneu, m, shift, tol, maxiter)
+    A = blas.daxpy(Bsneu, Asneu, a=shift)
+    # is_match = np.zeros((m),dtype=bool)
+    # for i in range(0,m):
+        # y = (Eval_neu[i]+shift) * np.matmul(Bsneu, Eveq_neu[:,i])
+        # z = np.matmul(A, Eveq_neu[:,i])
+        # print('\n',y,'\n',z,'\n')
+        # is_match[i] = np.allclose(y,z)
+    # print(is_match)
+    # print(all(is_match))
+    print(np.allclose(A @ Eveq_neu - Bsneu @ Eveq_neu @ (np.diag(Eval_neu)+shift * np.eye(m)), np.zeros((N**2, m))))
+
+    # Printer
+    print("## gen: stw\t\tArpack\t\tES-AR-ERROR")
+    for i in range(0, m):
+        print("%02i" %i, " %.6e" % Eval_neu[i], "\t %.6e" % W[i], "\t %.6e" % math.fabs(Eval_neu[i] - W[i]))
 if __name__ == '__main__':
     main()
