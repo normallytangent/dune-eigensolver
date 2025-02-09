d9eb979d46d0ee740906f0ab9c6c5309c02d09b4
Sanchi Vaishnavi <sanchi.vaishnavi@stud.uni-heidelberg.de>
2025-02-09 18:37:15 +0100

    Plotting Accuracy of Symmetric Generalized Eigenvalues N:28900 and 67600

--- src/plots/plot-ex02.py
+++ src/plots/plot-ex02.py
@@ -7,7 +7,7 @@ plt.rcParams["figure.figsize"] = (11.69,8.27)
 plt.rcParams['figure.constrained_layout.use'] = True
 
 # Export data
-def plot_frame(ax, status, n, nev, submethod, matrix):
+def plot_frame(ax, status, n, nev, acc, submethod, matrix):
     match status:
         case 'tol-std':
             tarry = []
@@ -40,30 +40,22 @@ def plot_frame(ax, status, n, nev, submethod, matrix):
 
         case 'tol-gen':
             tarry = []
-            # for x in map(str, np.arange(1,3)):
-            #     for y in map(str, np.arange(2,10,2)):
-            #         filename = '../measurements/N_40k_m_32_maxiter_4k_shift_1e-3_tol_'+y+'e-'+x+'_overlap_3_method_gen_submethod_'+submethod+'_'+matrix
-            #         dataset = np.loadtxt(filename, delimiter=' ',comments='#')
-            #         itr, time = find_iterations_time(filename)
-            #         time = '{0:.4g}'.format(float(time))
-            #         tarry.append(time)
-            #         custom_label = y+'e-'+x+', itr: '+itr+'t: '+time
-            #         ax.semilogy(np.arange(nev),dataset[:,-2],label=custom_label, marker='')
             for x in map(str, np.arange(1,5)):
-                for y in map(str, np.arange(1,2)):
-                    filename = '../simu/N_'+str(n)+'_m_'+str(nev)+'_maxiter_4k_shift_1e-3_tol_'+y+'e-'+x+'_overlap_3_method_gen_submethod_'+submethod+'_'+matrix
+                for y in map(str, np.arange(1,3)):
+                    filename = '../adapt_0_symmetric_gen/N_'+str(n)+'_m_'+str(nev)+'_acc_'+str(acc)+'_tol_'+y+'e-'+x+'_gen_'+submethod+'_'+matrix
                     dataset = np.loadtxt(filename, delimiter=' ',comments='#')
                     itr, time = find_iterations_time(filename)
                     time = '{0:.4g}'.format(float(time))
                     tarry.append(time)
-                    custom_label = y+'e-'+x+', itr: '+str(itr)+' t: '+time
+                    custom_label = y+'e-'+x #+', itr: '+str(itr)+' t: '+time
                     ax.semilogy(np.arange(nev),dataset[:,-2],label=custom_label, marker='')
 
             # Create legend
-            ax.set_title('problem: ' +matrix +', method: '+submethod)
+            # ax.set_title('Problem: ' +matrix +', method: '+submethod)
+            ax.set_title('Method: ' + ("GeneralizedInverse" if submethod=="ftw" else "GeneralizedSymmetricStewart"))
             ax.set_aspect('auto','box')
             labelLines(ax.get_lines(),align=False,zorder=2)
-            #ax.set_ylim(1e-17, 1e+0)
+            # ax.set_xlim(1e-17, 1e+0)
             #ax.legend(labels=tarry)
             # legend = ax.legend()
             # legend._legend_box.width = 100  # <-- Setting new width``
@@ -137,42 +129,43 @@ def beautify(fig, title, xlabel, ylabel, location):
     fig.savefig(location)
 
 def main():
-    n = 67601
-    nev = 32
+    n = 67600
+    nev = 144
+    acc = 4
     matrix = "neu" # "dir"
     prob = "tol-gen" # "tol-std"
-    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=False)
-    plot_frame(ax1, prob, n, nev, 'ftw', matrix)
-    plot_frame(ax2, prob, n, nev, 'stw', matrix)
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+    plot_frame(ax1, prob, n, nev, acc, 'ftw', matrix)
+    plot_frame(ax2, prob, n, nev, acc, 'stw', matrix)
 
-    title ='Accuracy of computed eigenvalues given various error tolerances \n n:'+str(n)+', m:'+str(nev)+', shift: 1e-3, regularization: 0, overlap: 3'
-    xlabel='Eigenvalues ranked smallest to largest'
-    ylabel='Error in eigenvalues'
-    location='img/'+prob+'-'+matrix+'-n-'+str(n)+'-m-'+str(nev)+'.pdf'
+    title ='Accuracy of '+str(nev)+' eigenvalues computed with error tolerances ranging from 2e-1 to 1e-4.\n Convergence governed by the smallest '+str(int(nev * acc/4))+ ' eigenvalues. Problem size: ' +str(n)+', shift: 1e-3, regularization: 0, overlap: 3'
+    xlabel='Eigenvalues ordered from smallest to largest'
+    ylabel='Error in eigenvalues w.r.t. Arpack\'s solution with error tolerance of 1e-14'
+    location='fig/adapt_0_symmetric_'+prob+'-'+matrix+'-n-'+str(n)+'-m-'+str(nev)+'-acc-'+str(acc)+'.pdf'
     beautify(fig,title,xlabel,ylabel,location)
 
-    nev = 32
-    matrix = "neu"
-    fig, (ax1, ax2) = plt.subplots(1, 2, sharex=False)
-    n_list = [40, 80]
-    for n in n_list:
-        n*=n
-        plot_frame_timeratio(ax1, 'tol-gen', n, nev, 'ftw', matrix)
-        plot_frame_timeratio(ax2, 'tol-gen', n, nev, 'stw', matrix)
+    # nev = 32
+    # matrix = "neu"
+    # fig, (ax1, ax2) = plt.subplots(1, 2, sharex=False)
+    # n_list = [40, 80]
+    # for n in n_list:
+    #     n*=n
+    #     plot_frame_timeratio(ax1, 'tol-gen', n, nev, 'ftw', matrix)
+    #     plot_frame_timeratio(ax2, 'tol-gen', n, nev, 'stw', matrix)
     
-    n = 67600
-    # # plot_frame_timeratio(ax1, 'tol-gen', n, nev, 'ftw', matrix)
-    plot_frame_timeratio(ax2, 'tol-gen', n, nev, 'stw', matrix)
+    # n = 67600
+    # # # plot_frame_timeratio(ax1, 'tol-gen', n, nev, 'ftw', matrix)
+    # plot_frame_timeratio(ax2, 'tol-gen', n, nev, 'stw', matrix)
 
-    n = 67601
-    plot_frame_timeratio(ax1, 'tol-gen', n, nev, 'ftw', matrix)
-    plot_frame_timeratio(ax2, 'tol-gen', n, nev, 'stw', matrix)
+    # n = 67601
+    # plot_frame_timeratio(ax1, 'tol-gen', n, nev, 'ftw', matrix)
+    # plot_frame_timeratio(ax2, 'tol-gen', n, nev, 'stw', matrix)
 
-    title ='Runtime ratios of eigensolver w.r.t arpack given error tolerances \n m:'+str(nev)+', shift: 1e-3, regularization: 0, overlap: 3'
-    xlabel='Tolerance'
-    ylabel='Runtime of Eigensolver relative to Arpack'
-    location='img/timeratio-gen-'+matrix+'-m-'+str(nev)+'.pdf'
-    beautify(fig,title,xlabel,ylabel,location)
+    # title ='Runtime ratios of eigensolver w.r.t arpack given error tolerances \n m:'+str(nev)+', shift: 1e-3, regularization: 0, overlap: 3'
+    # xlabel='Tolerance'
+    # ylabel='Runtime of Eigensolver relative to Arpack'
+    # location='fig/timeratio-gen-'+matrix+'-m-'+str(nev)+'.pdf'
+    # beautify(fig,title,xlabel,ylabel,location)
     # plt.show()
 
 if __name__ == "__main__":
