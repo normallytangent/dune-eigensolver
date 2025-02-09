e94d6bea998a06744ebf13f4faca54dec6192af1
Sanchi Vaishnavi <sanchi.vaishnavi@stud.uni-heidelberg.de>
2025-02-09 23:26:49 +0100

    Timing plots

--- src/plots/plot-ex02.py
+++ src/plots/plot-ex02.py
@@ -7,6 +7,35 @@ plt.rcParams["figure.figsize"] = (11.69,8.27)
 plt.rcParams['figure.constrained_layout.use'] = True
 
 # Export data
+def plot_convergence_ratio(ax, status, n, nev, acc, submethod, matrix):
+    match status:
+        case 'eig-ratio':
+            tarry = []
+            for x in map(str, np.arange(1,5)):
+                for y in map(str, range(2,0,-1)):
+                    filename = '../adapt_0_symmetric_gen/N_'+str(n)+'_m_'+str(nev)+'_acc_'+str(acc)+'_tol_'+y+'e-'+x+'_gen_'+submethod+'_'+matrix
+                    dataset = np.loadtxt(filename, delimiter=' ',comments='#')
+                    itr, time = find_iterations_time(filename)
+                    time = '{0:.4g}'.format(float(time))
+                    tarry.append(time)
+                    ratios = []
+                    ratios = [dataset[i,1] / dataset[i+1,1] for i in np.arange(0, nev-1)]  
+                    custom_label = y+'e-'+x #+', itr: '+str(itr)+' t: '+time
+                    ax.plot(np.arange(0, nev-1),ratios,label=custom_label, marker='')
+
+            # Create legend
+            ax.set_title('Method: ' + ("GeneralizedInverse" if submethod=="ftw" else "GeneralizedSymmetricStewart"))
+            ax.set_aspect('auto','box')
+            labelLines(ax.get_lines(),align=False,zorder=2)
+            ax.set_xlim(-1, nev+1)
+            # ax.legend(labels=tarry)
+            legend = ax.legend()
+            # legend._legend_box.width = 100  # <-- Setting new width``
+            # legend.get_frame().set_linewidth(0.0)
+
+        case _:
+            return "Try again!"
+
 def plot_frame(ax, status, n, nev, acc, submethod, matrix):
     match status:
         case 'tol-std':
@@ -41,7 +70,7 @@ def plot_frame(ax, status, n, nev, acc, submethod, matrix):
         case 'tol-gen':
             tarry = []
             for x in map(str, np.arange(1,5)):
-                for y in map(str, np.arange(1,3)):
+                for y in map(str, range(2,0,-1)):
                     filename = '../adapt_0_symmetric_gen/N_'+str(n)+'_m_'+str(nev)+'_acc_'+str(acc)+'_tol_'+y+'e-'+x+'_gen_'+submethod+'_'+matrix
                     dataset = np.loadtxt(filename, delimiter=' ',comments='#')
                     itr, time = find_iterations_time(filename)
@@ -65,30 +94,33 @@ def plot_frame(ax, status, n, nev, acc, submethod, matrix):
             return "Try again!"
 
 
-def plot_frame_timeratio(ax, status, n, nev, submethod, matrix):
+def plot_frame_timeratio(ax, status, n, nev, acc, submethod, matrix):
     match status:
-        case 'tol-gen':
+        case 'time-ratio':
             xarray = []
             yarray = []
+            acc_array = []
             for x in map(str, np.arange(1,5)):
-                for y in map(str, np.arange(1,0,-1)):
-                    filename = '../simu/N_'+str(n)+'_m_'+str(nev)+'_maxiter_4k_shift_1e-3_tol_'+y+'e-'+x+'_overlap_3_method_gen_submethod_'+submethod+'_'+matrix
+                for y in map(str, range(1,0,-1)):
+                    filename = '../adapt_0_symmetric_gen/N_'+str(n)+'_m_'+str(nev)+'_acc_'+str(acc)+'_tol_'+y+'e-'+x+'_gen_'+submethod+'_'+matrix
                     itr, time = find_iterations_time(filename)
                     arp_itr, arp_time = find_arpack_iterations_time(filename)
                     xarray.append(float(y+'e-'+x))
                     # print(float(time))
                     # print(float(arp_time))
                     yarray.append(float(time)/float(arp_time))
-                    strtime = '{0:.4g}'.format(float(time))
-                    arp_strtime = '{0:.2g}'.format(float(arp_time))
-                    ax.annotate(rf"$\frac{{{strtime}}}{{{arp_strtime}}}$", xy=(xarray[-1], yarray[-1]), xytext=(xarray[-1], yarray[-1]), textcoords='data', fontsize=10, ha=('right' if int(n)%80==0 else 'left'), \
-                            va=('top' if int(n)%40==0 else 'bottom'), rotation=10, annotation_clip=True, bbox=dict(boxstyle='round,pad=0.2', fc='none', alpha=0.1))
+                    # strtime = '{0:.4g}'.format(float(time))
+                    # arp_strtime = '{0:.2g}'.format(float(arp_time))
+                    # ax.annotate(rf"$\frac{{{strtime}}}{{{arp_strtime}}}$", xy=(xarray[-1], yarray[-1]), xytext=(xarray[-1], yarray[-1]), textcoords='data', fontsize=10, ha=('right' if int(n)%80==0 else 'left'), \
+                            # va=('top' if int(n)%40==0 else 'bottom'), rotation=10, annotation_clip=True, bbox=dict(boxstyle='round,pad=0.2', fc='none', alpha=0.1))
                     
-            ax.semilogx(xarray, yarray, marker='o', label= str(n))
+                    # acc_array.append(str(n) + ' ' +str(acc))
+            ax.semilogx(xarray, yarray, marker='o', label= str(n)+', '+str(int(nev*acc/4)) + ' eigvals' )
 
             # Create legend
-            ax.set_title('Problem: ' +matrix+ ', method: ' + ("Ftworth" if submethod=="ftw" else "Stewart"))
+            ax.set_title('Problem: '+ ("GeneralizedInverse" if submethod=="ftw" else "GeneralizedSymmetricStewart"))
             ax.set_aspect('auto','box')
+            # ax.legend(labels=acc_array)
             legend = ax.legend()
 
         case _:
@@ -129,44 +161,58 @@ def beautify(fig, title, xlabel, ylabel, location):
     fig.savefig(location)
 
 def main():
-    n = 67600
+    n_list = [28900, 67600]
+    acc_list = [1, 2, 3, 4]
     nev = 144
-    acc = 4
     matrix = "neu" # "dir"
-    prob = "tol-gen" # "tol-std"
-    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
-    plot_frame(ax1, prob, n, nev, acc, 'ftw', matrix)
-    plot_frame(ax2, prob, n, nev, acc, 'stw', matrix)
 
-    title ='Accuracy of '+str(nev)+' eigenvalues computed with error tolerances ranging from 2e-1 to 1e-4.\n Convergence governed by the smallest '+str(int(nev * acc/4))+ ' eigenvalues. Problem size: ' +str(n)+', shift: 1e-3, regularization: 0, overlap: 3'
-    xlabel='Eigenvalues ordered from smallest to largest'
-    ylabel='Error in eigenvalues w.r.t. Arpack\'s solution with error tolerance of 1e-14'
-    location='fig/adapt_0_symmetric_'+prob+'-'+matrix+'-n-'+str(n)+'-m-'+str(nev)+'-acc-'+str(acc)+'.pdf'
-    beautify(fig,title,xlabel,ylabel,location)
-
-    # nev = 32
-    # matrix = "neu"
-    # fig, (ax1, ax2) = plt.subplots(1, 2, sharex=False)
-    # n_list = [40, 80]
+    ###########################
+    #
+    #  Error in eigenvalues
+    #
+    ##########################
     # for n in n_list:
-    #     n*=n
-    #     plot_frame_timeratio(ax1, 'tol-gen', n, nev, 'ftw', matrix)
-    #     plot_frame_timeratio(ax2, 'tol-gen', n, nev, 'stw', matrix)
-    
-    # n = 67600
-    # # # plot_frame_timeratio(ax1, 'tol-gen', n, nev, 'ftw', matrix)
-    # plot_frame_timeratio(ax2, 'tol-gen', n, nev, 'stw', matrix)
+    #     for acc in acc_list:
+    #         prob = "tol-gen" # "tol-std"
+    #         fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+    #         plot_frame(ax1, prob, n, nev, acc, 'ftw', matrix)
+    #         plot_frame(ax2, prob, n, nev, acc, 'stw', matrix)
 
-    # n = 67601
-    # plot_frame_timeratio(ax1, 'tol-gen', n, nev, 'ftw', matrix)
-    # plot_frame_timeratio(ax2, 'tol-gen', n, nev, 'stw', matrix)
+    #         title ='Accuracy of '+str(nev)+' eigenvalues computed with error tolerances ranging from 2e-1 to 1e-4.\n Convergence governed by the smallest '+str(int(nev * acc/4))+ ' eigenvalues. Problem size: ' +str(n)+', shift: 1e-3, regularization: 0, overlap: 3'
+    #         xlabel='Eigenvalues ordered from smallest to largest'
+    #         ylabel='Error in eigenvalues w.r.t. Arpack\'s solution with error tolerance of 1e-14'
+    #         location='fig/adapt_0_symmetric_'+prob+'-'+matrix+'-n-'+str(n)+'-m-'+str(nev)+'-acc-'+str(acc)+'.pdf'
+    #         beautify(fig,title,xlabel,ylabel,location)
 
-    # title ='Runtime ratios of eigensolver w.r.t arpack given error tolerances \n m:'+str(nev)+', shift: 1e-3, regularization: 0, overlap: 3'
-    # xlabel='Tolerance'
-    # ylabel='Runtime of Eigensolver relative to Arpack'
-    # location='fig/timeratio-gen-'+matrix+'-m-'+str(nev)+'.pdf'
+
+    ###########################
+    #
+    #  Ratios of eigenvalues
+    #
+    ##########################
+    # prob = "eig-ratio" # "tol-std"
+    # fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+    # plot_convergence_ratio(ax1, prob, n, nev, acc, 'ftw', matrix)
+    # plot_convergence_ratio(ax2, prob, n, nev, acc, 'stw', matrix)
+
+    # title ='Theoretical convergence of '+str(nev)+' eigenvalues computed with error tolerances ranging from 2e-1 to 1e-4.\n Convergence governed by the smallest '+str(int(nev * acc/4))+ ' eigenvalues. Problem size: ' +str(n)+', shift: 1e-3, regularization: 0, overlap: 3'
+    # xlabel='Indices i corresponding to '+rf"${{\lambda_i}}$"+' from 1 to '+ str(nev)
+    # ylabel=rf"$\frac{{\lambda_i}}{{\lambda_{{i+1}}}}$"
+    # location='fig/adapt_0_symmetric_'+prob+'-'+matrix+'-n-'+str(n)+'-m-'+str(nev)+'-acc-'+str(acc)+'.pdf'
     # beautify(fig,title,xlabel,ylabel,location)
-    # plt.show()
+
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, sharey=False)
+    for n in n_list:
+        for acc in acc_list:
+            plot_frame_timeratio(ax1, 'time-ratio', n, nev, acc, 'ftw', matrix)
+            plot_frame_timeratio(ax2, 'time-ratio', n, nev, acc, 'stw', matrix)
+
+    title ='Runtime of Eigensolver normalised w.r.t Arpack given error tolerances \n m:'+str(nev)+', shift: 1e-3, regularization: 0, overlap: 3\n convergence controlled by some smallest eigenvalues'
+    xlabel='Tolerance'
+    ylabel='Runtime of Eigensolver relative to Arpack'
+    location='fig/adapt_0_symmetric_time-ratio-gen-'+matrix+'-m-'+str(nev)+'.pdf'
+    beautify(fig,title,xlabel,ylabel,location)
 
 if __name__ == "__main__":
         main()
